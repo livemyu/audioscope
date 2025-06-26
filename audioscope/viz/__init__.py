2261fae8f49d9d820cae1aed79eb67566e795155
Huang Yu <251566018+livemyu@users.noreply.github.com>
2025-06-26 13:52:12 +0800

    feat(viz): viz 子包汇出（颜色表/绘图）

--- audioscope/viz/__init__.py
+++ audioscope/viz/__init__.py
@@ -4,3 +4,20 @@ from __future__ import annotations
 
 from .ascii_render import heatmap, sparkline, waveform
 from .backends import available_backends, has_matplotlib
+from .colormaps import apply_colormap, available_colormaps, get_colormap
+from .plots import plot_chroma, plot_mel, plot_spectrogram, plot_waveform
+
+__all__ = [
+    "apply_colormap",
+    "available_backends",
+    "available_colormaps",
+    "get_colormap",
+    "has_matplotlib",
+    "heatmap",
+    "plot_chroma",
+    "plot_mel",
+    "plot_spectrogram",
+    "plot_waveform",
+    "sparkline",
+    "waveform",
+]
